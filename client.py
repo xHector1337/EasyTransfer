d738ef26f8d629170357deb076751ab0aa67d242
Umut <140441119+xHector1337@users.noreply.github.com>
2024-08-23 20:40:29 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,29 @@
 import socket
-
+import os
 port = int(input("Enter port:\n"))
 ip = str(input("Enter ip:\n"))
 passkey = "SKYFM"
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+mode = str(input("Enter uploading mode:(file or directory)\n"))
 
+def directoryUpload(path):
+    try:    
+        sock.connect((ip,port))
+        sock.send(passkey.encode())
+        os.chdir(path)
+        for i in os.listdir():
+            sock.send(i.encode())
+            with open(i,"rb") as f:
+                sock.sendfile(f)
+            sock.send(b"DONE")
+        print("All files are uploaded.")
+        exit(0)
+    except Exception as e:
+        print(e)
+        exit(1)    
+if mode == "directory":
+    path = str(input("Enter path of the directory you want to upload.\n"))
+    directoryUpload(path)                         
 try:
     sock.connect((ip,port))
     print(f"Connected to {ip}:{port}")
