e2ed0cc33147d4c74ff4dbd0f88129c75e3ad62e
Umut <140441119+xHector1337@users.noreply.github.com>
2024-08-23 12:52:11 +0300

    Update client.py

--- client.py
+++ client.py
@@ -3,14 +3,22 @@ import socket
 port = int(input("Enter port:\n"))
 ip = str(input("Enter ip:\n"))
 passkey = "SKYFM"
-message = "Hello man!"
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 try:
     sock.connect((ip,port))
     print(f"Connected to {ip}:{port}")
     sock.send(passkey.encode())
-    sock.send(message.encode())
+    sock.recv(1024)
+    filename = str(input(""))
+    sock.send(filename.encode())
+    with open(filename,"rb") as f:
+        data = f.read(1024)
+        while data:
+            sock.send(data)
+            data = f.read(1024)
+    sock.send(b'')
+    print("File sent.")        
     response = sock.recv(1024)
     print(f"Recieved {response.decode('utf-8')}")
 except Exception as e:
